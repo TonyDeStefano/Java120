f39351b3f0f4e01ee8f05da4fa916949ef1dba16
Tony DeStefano <tdestefano@funko.com>
2019-10-07 18:13:52 -0700

    Day 1: In class work.

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Hello World class!
+ */
+public class HelloWorld
+{
+    /**
+     * Staring point.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ mvn.sh
@@ -0,0 +1,1 @@
+mvn -e archetype:generate -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart -DgroupId=cp120.assignments -DartifactId=cp120 -Dversion=1 -Dpackage=uw.cp120.assignments.example
